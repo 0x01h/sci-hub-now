773387832dc82a1a3e98b7ce7e3aaaa12e75b18c
Gerry Chen <gchen328@gatech.edu>
2021-07-25 12:01:29 -0400

    update database root to a different branch

--- options.js
+++ options.js
@@ -188,7 +188,7 @@ function checkServerStatusHelper(testurl, i, ifOnline, ifOffline, ifWaiting) {
 }
 
 // Fetch data from database
-const databaseRoot = "https://raw.githubusercontent.com/gchenfc/sci-hub-now/master/data/";
+const databaseRoot = "https://raw.githubusercontent.com/gchenfc/sci-hub-now/release/v0.2.0/data/";
 // const databaseRoot = "data/"; // For local testing
 // fetch urls
 var links;
